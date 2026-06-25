a7c222fc51dced9f83b82a4b46b39a20c0273b94
Shiv-art623 <shivaniprajapati01062005@gmail.com>
2026-06-26 02:31:23 +0530

    1st way to Remove Duplicates

---
+++ RemoveDuplicatesFromAsortedList.cpp
@@ -0,0 +1,63 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Node{
+    public:
+    int data;
+    Node* next;
+Node(int i){
+    data= i;
+    next = NULL;
+}
+};
+//Remove by using etra space(arr)
+Node* RemoveDuplicates(Node* head){
+    if(head == NULL) return NULL;
+    vector<int> ans;
+    ans.push_back(head->data);
+    Node* curr = head->next;
+    while(curr){
+        if(ans[ans.size()-1] != curr->data){
+            ans.push_back(curr->data);
+        }
+        curr = curr->next;
+    }
+    curr = head;
+    int index = 0;
+    while(index<ans.size()){
+        curr->data = ans[index];
+        index++;
+        curr = curr->next;
+    }
+    int size = ans.size();
+    curr = head;
+    for(int i =1; i<ans.size(); i++){
+curr = curr->next;
+    }
+    curr->next = NULL;
+    return head;
+}
+
+
+int main(){
+int arr[] = {2,2,2,3,3,4,4};
+Node* head = NULL;
+for(int i =0; i<7; i++){
+if(head == NULL){
+    head = new Node(arr[i]);
+}
+else{
+            Node *tail = head;
+            while(tail->next!=NULL){
+                tail = tail->next;
+            }
+            Node *temp = new Node(arr[i]);     //tail->next = new Node(arr[i])
+            tail->next = temp;
+        }
+    }
+    Node* temp = RemoveDuplicates(head);
+    while(temp){
+        cout<<temp->data<<" ";
+        temp = temp->next;
+    }
+    return 0;
+}
