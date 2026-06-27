8f559dd9602d31bf22e08ab82ef00b1c7e19095f
Shiv-art623 <shivaniprajapati01062005@gmail.com>
2026-06-28 00:47:07 +0530

    Loop detection

--- DetectLoopInLL.cpp
+++ DetectLoopInLL.cpp
@@ -9,7 +9,41 @@ Node(int i){
     next = NULL;
 }
 };
-int main(){
+bool check(vector<Node*> visited, Node*curr){
+    for(int i =0; i<visited.size(); i++){
+        if(visited[i] == curr)
+            return 1;
+        }
+         return 0;
+    }
+    
 
+int main(){
+    int arr[] = {1,2,3,4,5,6,7,8,9,10};
+    int n = 10;
+    Node* head = new Node(arr[0]);
+    Node* temp = head;
+
+    Node* loopNode = NULL;
+     for(int i =1; i<n; i++){
+        temp->next = new Node(arr[i]);
+        temp = temp->next;
+
+        if(arr[i] == 4){
+            loopNode = temp;
+        }
+     }
+     temp->next =  loopNode;
+Node* curr = head;
+vector<Node*> visited;
+while(curr != NULL){
+   if(check(visited, curr)){
+    cout<<"Loop Found";
+    return 0;}
+
+   visited.push_back(curr);
+   curr = curr->next;
+}
+cout<<"Not Found";
     return 0;
 }
